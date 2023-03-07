2955ec00c1140e111ef0edf280521aff8e4b5726
Benjamin Treglown <ben@Benjamins-MacBook-Air.local>
2023-03-07 11:36:42 +1100

    Finished Home page design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,22 @@ const Home = () => {
   return (
     <div className='px-5 w-full h-screen flex justify-center items-center bg-cover bg-light-bg'>
 
+      {/* Title / Heading */}
       <div className='flex flex-col justify-center text-center'>
           <h2 className='font-bold'>Hello I'm <span className='text-[#CD49C0]'>Benjamin Treglown</span></h2>
           <h2 className='font-bold py-5'>and I'm a Front-end Developer based in <span>Melbourne, Australia</span></h2>
 
-
+          {/* Scrollable links */}
+          <div className='flex justify-between p-7 w-full'>
+            <a href="#" className='flex flex-col justify-center items-center'>
+              <p>Projects</p>
+              <img src="Black Arrow.png" alt="click here to scroll down the page"  className='h-6 rotate-90'/>
+            </a>
+            <a href="#" className='flex flex-col justify-center items-center'>
+              <p>About Me</p>
+              <img src="Black Arrow.png" alt="click here to scroll down the page"  className='h-6 rotate-90'/>
+            </a>
+          </div>
       </div>
     </div>
   )
