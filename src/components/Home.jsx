51c5bf59d9225d0f66137b37ec54781754bf30aa
Benjamin Treglown <ben@Benjamins-MacBook-Air.local>
2023-03-07 11:15:29 +1100

    First Commit, includes minimal homepage design

---
+++ src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='px-5 w-full h-screen flex justify-center items-center bg-cover bg-light-bg'>
+
+      <div className='flex flex-col justify-center text-center'>
+          <h2 className='font-bold'>Hello I'm <span className='text-[#CD49C0]'>Benjamin Treglown</span></h2>
+          <h2 className='font-bold py-5'>and I'm a Front-end Developer based in <span>Melbourne, Australia</span></h2>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
